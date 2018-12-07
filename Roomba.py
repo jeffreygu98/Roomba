9dcd22cf195dd54eff0a5eac0203d0d8d9139830
Jeffrey Gu <jwg33@duke.edu>
2018-12-07 17:14:43 -0500

    Final version

--- Roomba.py
+++ Roomba.py
@@ -5,6 +5,7 @@ Created on Nov 28, 2018
 '''
 import turtle
 import random
+import math
 import time
 from easygui import *
 import numpy as np
@@ -24,27 +25,10 @@ Roomba = turtle.Turtle()
 Roomba.pencolor("white")
 Roomba.pensize(50)
 Roomba.shape(image)
-# Roomba.speed(10)
-
-
-
-def move(turtle, distance):
-    turtle.speed(0)
-    distance = float(distance)
-    for i in range(0, abs(int(distance/2))):
-        x, y = turtle.pos()
-        print(x)
-        print(y)
-        if (-width/2 < x < width/2) and  (-height/2 < y < height/2):
-            if distance < 0:
-                turtle.forward(-2)
-            else:           
-                turtle.forward(2)
-        else:
-            turtle.undo()
-            break
+Roomba.speed(0)
 
 def custom(turtle):
+    global num_uncleaned
     # sets the angle of movement
     angle = float(integerbox(msg="Please put the angle (-180 to 180) you wish the Roomba to travel with.", lowerbound = -180, upperbound = 180))
     
@@ -56,14 +40,29 @@ def custom(turtle):
             break
         current = turtle.heading()
         x, y = turtle.pos()
+        coord = (int(x)+249,int(y)+249)
+        print("current coords: ",int(coord[0]),int(coord[1]))
+        for i in range(50):
+            for j in range(50):
+                xclean = coord[0]+i
+                yclean = coord[1]+j
+                if 0<=xclean<500 and 0<=yclean<500 and (i**2+j**2 <= 30**2):
+                    if not clean_grid[xclean,yclean]:
+                        clean_grid[xclean,yclean] = True
+                        num_uncleaned-=1
+        print("num of coords: ",num_uncleaned)
         if (-width/2 < x < width/2) and  (-height/2 < y < height/2):
                 turtle.forward(2)
         else:
             turtle.undo()
             turtle.setheading(current+angle)
+    
+    # Return Roomba to its home; final goodbyes
+    home(Roomba, homex, homey)
+    msgbox("Roomba has cleaned "+str(int((((250000-num_uncleaned)/250000)*100)))+"% of your home in "+str(int(length))+" seconds. Good-bye!")
 
 def freeroam(turtle):
-    
+    global num_uncleaned
     # sets how long the Roomba runs
     length = float(integerbox("How long would you like the Roomba to run (in seconds)?"))
     start = time.time()
@@ -72,20 +71,37 @@ def freeroam(turtle):
             break
         current = turtle.heading()
         x, y = turtle.pos()
+        coord = (int(x)+249,int(y)+249)
+        print("current coords: ",int(coord[0]),int(coord[1]))
+        for i in range(50):
+            for j in range(50):
+                xclean = coord[0]+i
+                yclean = coord[1]+j
+                if 0<=xclean<500 and 0<=yclean<500 and (i**2+j**2 <= 30**2):
+                    if not clean_grid[xclean,yclean]:
+                        clean_grid[xclean,yclean] = True
+                        num_uncleaned-=1
+        print("num of coords: ",num_uncleaned)
         if (-width/2 < x < width/2) and  (-height/2 < y < height/2):
                 turtle.forward(2)
         else:
             turtle.undo()
             angle = random.randint(-180,180)
             turtle.setheading(current+angle) 
+    
+    # Return Roomba to its home; final goodbyes
+    home(Roomba, homex, homey)
+    msgbox("Roomba has cleaned "+str(int((((250000-num_uncleaned)/250000)*100)))+"% of your home in "+str(int(length))+" seconds. Good-bye!")
 
 def complete(turtle):
     global num_uncleaned
     clean = False
+    cleanage = integerbox("What percent of the floor would you like Roomba to clean (0-95)", lowerbound = 0, upperbound = 95)
+    start = time.time()
     while True:
-        if(num_uncleaned == 0):
+        if(int((((250000-num_uncleaned)/250000)*100)) > (cleanage-1)):
             break
-            
+                
         current = turtle.heading()
         x, y = turtle.pos()
         coord = (int(x)+249,int(y)+249)
@@ -94,12 +110,11 @@ def complete(turtle):
             for j in range(50):
                 xclean = coord[0]+i
                 yclean = coord[1]+j
-                if 0<=xclean<500 and 0<=yclean<500 and (i**2+j**2 <= 25**2):
+                if 0<=xclean<500 and 0<=yclean<500 and (i**2+j**2 <= 30**2):
                     if not clean_grid[xclean,yclean]:
                         clean_grid[xclean,yclean] = True
                         num_uncleaned-=1
         print("num of coords: ",num_uncleaned)
-        print(250000-np.sum(clean_grid))
 #         if(len(coordinates)<100):
 #             clean = True
         if (-width/2 < x < width/2) and (-height/2 < y < height/2):
@@ -108,7 +123,12 @@ def complete(turtle):
             turtle.undo()
             angle = random.randint(-180,180)
             turtle.setheading(current+angle)     
+    end = time.time()
     
+    # Return Roomba to its home; final goodbyes
+    home(Roomba, homex, homey)
+    msgbox("Roomba has cleaned "+str(int((((250000-num_uncleaned)/250000)*100)))+"% of your home in "+str(int(end-start))+" seconds. Good-bye!")
+
 def home(turtle, x, y):
     turtle.penup()
     turtle.speed(1)
@@ -129,18 +149,14 @@ if __name__ == '__main__':
        
     # Specify running mode for Roomba
        
-    type = buttonbox(msg="Please choose Roomba's running mode.", title = "Running mode", choices =["Free-roam (random bump)", "Custom bump angle", "Deep clean"])
+    type = buttonbox(msg="Please choose Roomba's running mode.", title = "Running mode", choices =["Free-roam (random bump)", "Custom bump angle", "Custom percentage"])
    
     if type=="Free-roam (random bump)":
         freeroam(Roomba)
     if type =="Custom bump angle":
         custom(Roomba)
-    if type == "Deep clean":
+    if type == "Custom percentage":
         complete(Roomba)
-    
-    # Return Roomba to its home; final goodbyes
-    home(Roomba, homex, homey)
-    msgbox("Your home is now clean. Good-bye!")
     
     # Exit the program with click
     wn.exitonclick()
